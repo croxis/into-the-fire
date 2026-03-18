ea1fd1c6bc17db3c1781f1969c1f500b40892ab1
David Radford <croxis@gmail.com>
2026-03-18 15:52:52 -0700

    Update to 4.6 and moved some of babylon 5 to binary files

--- ship_systems/pilots/fleet_property_updater.gd
+++ ship_systems/pilots/fleet_property_updater.gd
@@ -6,23 +6,25 @@ extends PropertyUpdater
 ## Add your configurable properties here.  They can be passed in from
 ## the GdPAIBehaviorConfig that instantiates the PropertyUpdater.
 ## How many ships are subservent in the fleet.
-var total_pilots: int = 0
+var unlaunched_pilots: int = 0
 var launched_pilots: int = 0
+var desired_pilots: int = 0
 var total_fighters: int = 0
 var launched_fighters: int = 0
 var total_capitals: int = 0
 var total_stations: int = 0
 
 
-func _init(p_total_pilots: int = 0) -> void:
+func _init(p_desired_pilots: int = 0) -> void:
 	# Assign any parameters here.
-	self.total_pilots = p_total_pilots
+	self.desired_pilots = p_desired_pilots
 
 
 # Override
 func initialize(agent: GdPAIAgent) -> void:
 	# Set up initial property values with the agent.
-	agent.blackboard.set_property("total_pilots", total_pilots)
+	agent.blackboard.set_property("desired_pilots", desired_pilots)
+	agent.blackboard.set_property("unlaunched_pilots", unlaunched_pilots)
 	agent.blackboard.set_property("launched_pilots", launched_pilots)
 	agent.blackboard.set_property("total_fighters", total_fighters)
 	agent.blackboard.set_property("launched_fighters", launched_fighters)
@@ -38,7 +40,7 @@ func update_properties(
 	# Update agent properties that need to be maintained as part of this behavior.
 	#var runtime_value: float = agent.blackboard.get_property("example_parameter")
 	#agent.blackboard.set_property("example_parameter", runtime_value + delta)
-	agent.blackboard.set_property("total_pilots", total_pilots)
+	agent.blackboard.set_property("unlaunched_pilots", unlaunched_pilots)
 	agent.blackboard.set_property("launched_pilots", launched_pilots)
 	agent.blackboard.set_property("total_fighters", total_fighters)
 	agent.blackboard.set_property("launched_fighters", launched_fighters)
--- ships/earth alliance/babylon 5/rotation.gd
+++ ships/earth alliance/babylon 5/rotation.gd
@@ -1,4 +1,4 @@
-@tool
+#@tool
 extends MeshInstance3D
 
 @export var rpm = 0.75
